3b9d7c1e29b0f13b02cd9ae6b0b370a130874c69
TreyRuffy <treyruffy@gmail.com>
2017-07-19 17:23:31 -0600

    1.1.4
    + New dependency: commons-io
    + Added an update checker for console and players with permission
    tdj.updates

---
+++ src/me/treyruffy/treysdoublejump/Updater/UpdateChecker.java
@@ -0,0 +1,36 @@
+package me.treyruffy.treysdoublejump.Updater;
+
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import me.treyruffy.treysdoublejump.TreysDoubleJump;
+
+public class UpdateChecker {
+
+	final static String VERSION_URL = "https://api.spiget.org/v2/resources/19630/versions?size=" + Integer.MAX_VALUE;
+	final static String DESCRIPTION_URL = "https://api.spiget.org/v2/resources/19630/updates?size=" + Integer.MAX_VALUE;
+
+	public static Object[] getLastUpdate() {
+        try {
+            JSONArray versionsArray = (JSONArray) JSONValue.parseWithException(IOUtils.toString(new URL(String.valueOf(VERSION_URL)), "UTF-8"));
+            String lastVersion = ((JSONObject) versionsArray.get(versionsArray.size() - 1)).get("name").toString();
+
+            if ((Integer.parseInt(lastVersion.replaceAll("\\.","")) > (Integer.parseInt(TreysDoubleJump.getPlugin(TreysDoubleJump.class).getDescription().getVersion().replaceAll("\\.",""))))) {
+                JSONArray updatesArray = (JSONArray) JSONValue.parseWithException(IOUtils.toString(new URL(String.valueOf(DESCRIPTION_URL)), "UTF-8"));
+                String updateName = ((JSONObject) updatesArray.get(updatesArray.size() - 1)).get("title").toString();
+   
+                Object[] update = {lastVersion, updateName};
+                return update;
+            }
+        }
+        catch (Exception e) {
+            return new String[0];
+        }
+		return new String[0];
+    }
+	
+}
---
+++ src/me/treyruffy/treysdoublejump/Updater/Updates.java
@@ -0,0 +1,66 @@
+package me.treyruffy.treysdoublejump.Updater;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import me.treyruffy.treysdoublejump.ConfigManager;
+import me.treyruffy.treysdoublejump.TreysDoubleJump;
+
+public class Updates implements Listener {
+
+	public static void updateCheck(){
+		if (ConfigManager.getConfig().getBoolean("Updates.Check")){
+			UpdateChecker.getLastUpdate();
+			Object[] updates = UpdateChecker.getLastUpdate();
+			if (updates.length == 2){
+				ConsoleCommandSender console = Bukkit.getConsoleSender();
+				console.sendMessage(ChatColor.AQUA + "+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=+");
+				console.sendMessage(ChatColor.GREEN + "There is a new update for");
+				console.sendMessage(ChatColor.GREEN + "Trey's Double Jump");
+				console.sendMessage(ChatColor.RED + "Download at:");
+				console.sendMessage(ChatColor.LIGHT_PURPLE + "https://www.spigotmc.org/resources/19630/");
+				console.sendMessage(ChatColor.AQUA + "+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=+");
+				if (ConfigManager.getConfig().getBoolean("Updates.TellPlayers")){
+					for (Player p : Bukkit.getOnlinePlayers()){
+						if (p.hasPermission("tdj.updates")){
+							p.sendMessage(ChatColor.AQUA + "+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=+");
+							p.sendMessage(ChatColor.GREEN + "There is a new update for");
+							p.sendMessage(ChatColor.GREEN + "Trey's Double Jump");
+							p.sendMessage(ChatColor.RED + "Download at:");
+							p.sendMessage(ChatColor.LIGHT_PURPLE + "https://www.spigotmc.org/resources/19630/");
+							p.sendMessage(ChatColor.AQUA + "+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=+");
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	@EventHandler
+	public void onJoin(PlayerJoinEvent e){
+		if (ConfigManager.getConfig().getBoolean("Updates.Check")){
+			if (ConfigManager.getConfig().getBoolean("Updates.TellPlayers")){
+				final Player p = e.getPlayer();
+				if (p.hasPermission("tdj.updates")){
+					Bukkit.getScheduler().scheduleSyncDelayedTask(TreysDoubleJump.getPlugin(TreysDoubleJump.class), new Runnable() {
+						@Override
+						public void run() {
+							p.sendMessage(ChatColor.AQUA + "+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=+");
+							p.sendMessage(ChatColor.GREEN + "There is a new update for");
+							p.sendMessage(ChatColor.GREEN + "Trey's Double Jump");
+							p.sendMessage(ChatColor.RED + "Download at:");
+							p.sendMessage(ChatColor.LIGHT_PURPLE + "https://www.spigotmc.org/resources/19630/");
+							p.sendMessage(ChatColor.AQUA + "+=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=+");
+						}
+					}, 4);
+				}
+			}
+		}
+	}
+	
+}
