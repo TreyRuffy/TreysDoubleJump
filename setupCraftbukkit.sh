024aef442a2d3f3958bc2959175c9ead3ed6d896
TreyRuffy <treyruffy@users.noreply.github.com>
2020-10-10 21:18:10 -0600

    1.16.3 and CraftBukkit Script Fix

--- setupCraftbukkit.sh
+++ setupCraftbukkit.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 # Created by TreyRuffy for Trey's Double Jump
+#
 # Feel free to use as you need for any of your projects.
 # In order to make this execute commands, read the .semaphore/sempaphore.yml and the
 # .github/workflows/codeql-analysis.yml files
+
+# First argument is the version of Spigot (eg. 1.16.2)
+# Second argument is if you should force update the Spigot files
+#   Set as true for yes
+# Run bash setupCraftbukkit.sh 1.16.2 true
 if [ $# -eq 0 ]; then
     echo "Missing arguments"
     exit 1
 fi
 
-if [ ! -f "$HOME/craftbukkit/BuildTools.jar" ]; then
+forceUpdate=false;
+
+if [ $# -eq 2 ] && [ "$2" == 'true' ]; then
+  forceUpdate=true;
+fi
+
+if [ ! -f "$HOME/craftbukkit/BuildTools.jar" ] || [ $forceUpdate == true ]; then
 
 mkdir -p "$HOME"/craftbukkit
 echo "Downloading Build Tools for Minecraft version $1"
@@ -20,7 +32,7 @@ cd "$HOME"/craftbukkit || exit
 chmod +rx BuildTools.jar
 
 # Build spigot
-if [ ! -f "$HOME/.m2/repository/org/bukkit/craftbukkit/$1-R0.1-SNAPSHOT/craftbukkit-$1-R0.1-SNAPSHOT.jar" ]; then
+if [ ! -f "$HOME/.m2/repository/org/bukkit/craftbukkit/$1-R0.1-SNAPSHOT/craftbukkit-$1-R0.1-SNAPSHOT.jar" ] || [ $forceUpdate == true ]; then
 echo "Building CraftBukkit using Spigot Build Tools for Minecraft version $1"
 java -jar BuildTools.jar --rev "$1" --compile CRAFTBUKKIT,SPIGOT
 
